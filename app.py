37cc017e19fb695d1e3392503420514a9e3656dc
thiago puentes <thiago_008@hotmail.com>
2025-05-31 02:17:25 -0300

    Adicionar novas rotas e modelos para seções educacionais, de QA e dicionário

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-# Importamos la clase Flask del paquete flask
 from flask import Flask, render_template
 
-# Creamos una instancia de la aplicación Flask
-# El parámetro __name__ indica que este archivo es el punto principal de la app
 app = Flask(__name__)
 
-# Definimos una RUTA
-# Esto significa: cuando alguien acceda a la URL raíz '/' ejecutará la función 'home'
 @app.route('/')
 def home():
-    # Esta función devuelve una respuesta que el navegador mostrará
     return render_template("index.html")
 
-# Este bloque asegura que el servidor solo se ejecuta si el script se corre directamente
-# y no si es importado desde otro archivo
+# Página de Educação
+@app.route('/educational')
+def educational():
+    return render_template("educational.html")
+
+# Página de Perguntas e Respostas
+@app.route('/qa')
+def qa():
+    return render_template("qa.html")
+
+# Página do Dicionário
+@app.route('/dictionary')
+def dictionary():
+    return render_template("dictionary.html")
+
 if __name__ == '__main__':
-    # Levantamos el servidor Flask en modo debug
-    # Esto permite que veamos los errores detalladamente y recargue automáticamente al cambiar código
     app.run(debug=True)
