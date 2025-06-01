e9f1bb55bbb1831941cdc3ee5b7486accfa5f849
TalesVitor <talesvitorss@gmail.com>
2025-05-31 22:21:16 -0300

    adicionar e editar termos adicionados

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from google import genai
 
 app = Flask(__name__)
@@ -36,10 +36,39 @@ def qa():
         answer = get_gemini_response(question)
     return render_template("qa.html", answer=answer)
 
-# Página do Dicionário
+# Página principal do dicionário
 @app.route('/dictionary')
 def dictionary():
-    return render_template("dictionary.html")
+    return render_template("dictionary.html", terms=terms)
+
+# Adicionar termo
+@app.route('/add', methods=['GET', 'POST'])
+def add_term():
+    if request.method == 'POST':
+        term = request.form['term']
+        definition = request.form['definition']
+        terms[term] = definition
+        return redirect(url_for('dictionary'))
+    return render_template("add_term.html")
+
+# Editar termo
+@app.route('/edit/<term>', methods=['GET', 'POST'])
+def edit_term(term):
+    if request.method == 'POST':
+        new_term = request.form['term']
+        definition = request.form['definition']
+        # Remover termo antigo se foi renomeado
+        if new_term != term:
+            del terms[term]
+        terms[new_term] = definition
+        return redirect(url_for('dictionary'))
+    return render_template("edit_term.html", term=term, definition=terms[term])
+
+# Deletar termo
+@app.route('/delete/<term>', methods=['POST'])
+def delete_term(term):
+    terms.pop(term, None)
+    return redirect(url_for('dictionary'))
 
 if __name__ == '__main__':
     app.run(debug=True)
